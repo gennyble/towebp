950113e9ad7f62667ad4157f7c977b4d01f9fb2b
Brad Alfirevic <brad@genbyte.dev>
2021-03-04 15:40:07 -0600

    Change cli to act more like cat

--- src/cli.rs
+++ src/cli.rs
@@ -3,8 +3,7 @@ use std::path::PathBuf;
 
 pub struct CliArgs {
 	pub quality: f32,
-	pub ipath: PathBuf,
-	pub opath: PathBuf
+	pub files: Vec<(PathBuf, PathBuf)>
 }
 
 impl CliArgs {
@@ -15,7 +14,7 @@ impl CliArgs {
     }
 
 	fn usage(program: &str, opts: Options) -> String {
-        let brief = format!("Usage: {} [options]", program);
+        let brief = format!("Usage: {} [options] FILE...", program);
         format!("{}", opts.usage(&brief))
     }
 
@@ -25,8 +24,6 @@ impl CliArgs {
 
 		let mut opts = Options::new();
 		opts.optopt("q", "quality", "Quality of WebP image: 0-100", "FLOAT");
-		opts.reqopt("i", "ipath", "Path you want to convert to WebP", "PATH");
-		opts.optopt("o", "opath", "Path you want the WebP to be placed", "PATH");
 		let matches = match opts.parse(&args[1..]) {
 			Ok(m) => m,
 			Err(e) => {
@@ -36,16 +33,17 @@ impl CliArgs {
 		};
 
 		let quality = matches.opt_get_default("quality", 80f32).expect("Failed to parse quality");
-		let ipath: PathBuf = PathBuf::from(matches.opt_str("ipath").expect("ipath is required, how did we get here?"));
-		let opath: PathBuf = match matches.opt_str("opath").map(|s| PathBuf::from(s)) {
-			Some(p) => p,
-			None => Self::calculate_opath(&ipath)
-		};
+		
+		let mut files = vec![];
+		for inputs in matches.free {
+			let ipath = PathBuf::from(inputs);
+			let opath = Self::calculate_opath(&ipath);
+			files.push((ipath, opath));
+		}
 
 		Some(Self {
 			quality,
-			ipath,
-			opath
+			files
 		})
 	}
 
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use cli::CliArgs;
 use image::io::Reader as ImageReader;
 use webp::Encoder as WebpEncoder;
 use std::fs;
+use std::path::PathBuf;
 
 fn main() {
 	let cli = match CliArgs::new() {
@@ -11,11 +12,28 @@ fn main() {
 		None => return
 	};
 
-    let img = ImageReader::open(cli.ipath)
-        .expect("Failed to open image")
-        .decode()
-        .expect("Failed to decode image");
+    for (ipath, opath) in cli.files.iter() {
+		to_webp(&ipath, &opath, cli.quality);
+	}
+}
 
-	let webp = WebpEncoder::from_image(&img).encode(cli.quality);
-	fs::write(cli.opath, webp.iter()).expect("Failed to write webp to file");
+fn to_webp(ipath: &PathBuf, opath: &PathBuf, quality: f32) {
+    let img = match ImageReader::open(ipath) {
+		Ok(read_img) => match read_img.decode() {
+			Ok(decoded) => decoded,
+			Err(_e) => {
+				eprintln!("towebp: Failed to decode image '{}'", ipath.to_string_lossy());
+				return;
+			}
+		},
+		Err(_e) => {
+			eprintln!("towebp: Failed to read image '{}'", ipath.to_string_lossy());
+			return;
+		}
+	};
+
+	let webp = WebpEncoder::from_image(&img).encode(quality);
+	if let Err(_) = fs::write(opath, webp.iter()) {
+		eprintln!("towebp: Failed to write out webp for '{}'", ipath.to_string_lossy());
+	}
 }
